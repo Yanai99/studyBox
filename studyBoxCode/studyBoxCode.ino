116c2039f21291eb833a1a966dd34987102b87a3
Yanai99 <yanaiparan@gmail.com>
2023-05-07 18:34:43 +0300

    configured buttons

--- studyBoxCode/studyBoxCode.ino
+++ studyBoxCode/studyBoxCode.ino
@@ -9,14 +9,18 @@
 LiquidCrystal lcd(7, 8, 2, 3, 4, 5);
 
 int contrastPin = 9; // Replace with the actual pin number connected to the contrast pin of your LCD screen
+int timeMinutes = 1;
 int timeHours = 0;
-String time = "00:40"; 
+char timeString[6];
+bool changeTime = false;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(contrastPin, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_END, INPUT_PULLUP);
+  pinMode(BUTTON_UP, INPUT_PULLUP);
+  pinMode(BUTTON_DOWN, INPUT_PULLUP);
 
   analogWrite(contrastPin, 100); // adjust the value to change the contrast
   // set up the LCD's number of columns and rows
@@ -28,17 +32,89 @@ void setup() {
 
 void loop() 
 {
-  while(digitalRead(BUTTON_PIN))
+  lcd.clear();
+  lcd.print("Time: ");
+  setTimeString(timeHours, timeMinutes);
+  //String time = String(timeString);
+  lcd.print(timeString);
+
+  while(digitalRead(BUTTON_END))
   {
+    if(changeTime)
+    {
+      lcd.clear();
+      lcd.print("Time: ");
+      setTimeString(timeHours, timeMinutes);
+      lcd.print(timeString);
+      changeTime = false;
+    }
+
     if(!digitalRead(BUTTON_UP))
     {
-
+      IncreaseTime();
+      changeTime = true;
+      delay(500);
     }
     if(!digitalRead(BUTTON_DOWN))
     {
-
+      DecreaseTime();
+      changeTime = true;
+      delay(500);
     }
   }
-  Serial.println(digitalRead(BUTTON_PIN));
-  delay(100);
+  LockBox();
+  /*lcd.clear();
+  lcd.print("Done!");*/
+  delay(3000);
+}
+
+void IncreaseTime()
+{
+  if(timeMinutes == 50)
+  {
+    timeMinutes = 0;
+    timeHours = 1;
+  }
+  else
+    timeMinutes += 10;
+  
+}
+
+void DecreaseTime()
+{
+  if(timeMinutes == 00)
+  {
+    timeMinutes = 50;
+    timeHours = 0;
+  }
+  else
+    timeMinutes -= 10;
+}
+
+void LockBox()
+{
+  int i;
+  int minutesDelay = timeMinutes + (timeHours*60);
+  lcd.clear();
+  lcd.print("Locked, Study!");
+  lcd.setCursor(0, 1);
+  lcd.print("Time Left:");
+  for( i =0; i <minutesDelay ; i++)
+  {
+    lcd.clear();
+    lcd.print("Locked, Study!");
+    lcd.setCursor(0, 1);
+    lcd.print("Time Left:");
+    lcd.print(minutesDelay - i);
+    lcd.print(" Min");
+    delay(60000);  
+  }
+  lcd.clear();
+  lcd.print("Cngratz!");
+
+}
+
+void setTimeString(int hours, int minutes)
+{
+  sprintf(timeString, "%02d:%02d", hours, minutes);
 }
