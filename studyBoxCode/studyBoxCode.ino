cffc01a248f10d9804883c2f2f737eed8689e85c
Yanai99 <yanaiparan@gmail.com>
2023-05-09 01:26:03 +0300

    added LED and wait state

--- studyBoxCode/studyBoxCode.ino
+++ studyBoxCode/studyBoxCode.ino
@@ -5,6 +5,7 @@ Servo myServo;
 #define BUTTON_END 13
 #define BUTTON_UP 12
 #define BUTTON_DOWN 11
+int ledPin = 6; // the digital pin the LED is connected to
 
 // if button is pressed than 1 else 0
 
@@ -21,13 +22,15 @@ void setup() {
   Serial.begin(9600);
 
   myServo.attach(10); // attach the servo to pin 10
-   myServo.write(90);
+   myServo.write(0);
   pinMode(contrastPin, OUTPUT);
   pinMode(BUTTON_END, INPUT_PULLUP);
   pinMode(BUTTON_UP, INPUT_PULLUP);
   pinMode(BUTTON_DOWN, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT); // set the LED pin as an output
 
   analogWrite(contrastPin, 100); // adjust the value to change the contrast
+
   // set up the LCD's number of columns and rows
   lcd.begin(16, 2);
   // Print a message to the LCD.
@@ -37,6 +40,7 @@ void setup() {
 
 void loop() 
 {
+  digitalWrite(ledPin, HIGH); // turn on the LED
   lcd.clear();
   lcd.print("Time: ");
   setTimeString(timeHours, timeMinutes);
@@ -67,7 +71,9 @@ void loop()
       delay(500);
     }
   }
+  CountDown();
   LockBox();
+  myServo.write(0);
   /*lcd.clear();
   lcd.print("Done!");*/
   delay(3000);
@@ -75,7 +81,7 @@ void loop()
 
 void IncreaseTime()
 {
-  if(timeMinutes == 50)
+  if(timeMinutes >= 50)
   {
     timeMinutes = 0;
     timeHours = 1;
@@ -87,7 +93,7 @@ void IncreaseTime()
 
 void DecreaseTime()
 {
-  if(timeMinutes == 00)
+  if(timeMinutes <= 00)
   {
     timeMinutes = 50;
     timeHours = 0;
@@ -96,15 +102,36 @@ void DecreaseTime()
     timeMinutes -= 10;
 }
 
+void CountDown() // Gives user 15 seconds to put inside the phone
+{
+  int i;
+
+  for( i =0; i <15 ; i++)
+  {
+    lcd.clear();
+    lcd.print("Time Left:");
+    lcd.setCursor(0, 1);
+    lcd.print(15-i);
+    lcd.print(" Sec");
+    delay(1000);  
+  }
+  return;
+}
+
+
 void LockBox()
 {
   int i;
   int minutesDelay = timeMinutes + (timeHours*60);
   int secondsDelay = minutesDelay * 60;
+  int minToDisplay = secondsDelay/60 - i/60;
+  digitalWrite(ledPin, LOW); // turn off the LED
   lcd.clear();
   lcd.print("Locked, Study!");
   lcd.setCursor(0, 1);
   lcd.print("Time Left:");
+  lcd.print((secondsDelay/60) - i/60);
+  myServo.write(160);
   for( i =0; i <secondsDelay ; i++)
   {
     if(!digitalRead(BUTTON_DOWN) && !digitalRead(BUTTON_UP))
@@ -112,15 +139,20 @@ void LockBox()
       if(GiveUp())
         return;
     }
-    lcd.clear();
-    lcd.print("Locked, Study!");
-    lcd.setCursor(0, 1);
-    lcd.print("Time Left:");
-    lcd.print((secondsDelay/60) - i/60);
-    lcd.print(" Min");
+    if(((secondsDelay/60) - i/60) != minToDisplay)
+    {
+      minToDisplay = (secondsDelay/60) - i/60;
+     lcd.clear();
+     lcd.print("Locked, Study!");
+     lcd.setCursor(0, 1);
+     lcd.print("Time Left:");
+     lcd.print((secondsDelay/60) - i/60);
+     lcd.print(" Min");
+    }
+
     delay(1000);  
   }
-  myServo.write(180);
+  myServo.write(160);
   lcd.clear();
   lcd.print("Congratz!");
 
